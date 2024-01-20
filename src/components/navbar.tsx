df1f11b111ecc40fe7833b027068453768d36bfa
alyona-nad <116522266+alyona-nad@users.noreply.github.com>
2024-01-21 02:55:15 +0300

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,12 +8,9 @@ function BasicExample() {
     <Navbar bg="light" expand="lg">
       <Container fluid className="new flex-column" style={{width:"100%"}}>
         <Navbar.Brand href="#home" className="brand text-center" style={{fontSize:"2em"}}>Производство красок</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="w-100 d-flex justify-content-center ">
           <Nav className="me-auto">
             <Nav.Link href="/RIP_frontend/" className="li mx-3" style={{flex:"1"}}>Каталог</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
       </Container>
 </Navbar>
   );
